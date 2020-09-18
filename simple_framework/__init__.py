e7999e752d3c9bbcd092e78646e8abcaab52b94d
Marcin <marcin.l.czelej@gmail.com>
2020-09-18 09:29:22 +0000

    feat:SMall fixes

--- simple_framework/__init__.py
+++ simple_framework/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1.5"
+__version__ = "0.0.1.6"
 
 from simple_framework.trainer import Trainer, SimpleFrameworkWrapper
 
--- simple_framework/backends/simple_backend.py
+++ simple_framework/backends/simple_backend.py
@@ -25,8 +25,6 @@ class SimpleBackend(BackendBase):
         self.global_step = 0
         self.settings = settings
 
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
         self.checkpointer = Checkpoint_saver(
             checkpoints_dir=self.settings["save_path"],
             description=self.settings["description"],
@@ -42,7 +40,7 @@ class SimpleBackend(BackendBase):
 
     def train_step(self, data, step):
         self.optimizer.zero_grad()
-        loss, metrics = self.model.train_step(data.to(self.device))
+        loss, metrics = self.model.train_step(data)
 
         # creating metrics container first time we see it
         if not self.metric_container:
@@ -182,7 +180,7 @@ class SimpleBackend(BackendBase):
                     dl_iter = iter(validation_dataloader)
                     data = next(dl_iter)
 
-                loss, metrics = self.model.train_step(data.to(self.device))
+                loss, metrics = self.model.train_step(data)
 
                 # add to average meter for loss
                 validation_sum_loss.update(loss.item(), self.settings["validation_batch_size"])
