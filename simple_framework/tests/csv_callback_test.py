0ac19d131672d20bda895ba9ef83af8327ac21b2
Marcin <marcin.l.czelej@gmail.com>
2020-09-21 12:45:45 +0000

    feat:pytest tests splitted and renamed

--- simple_framework/tests/csv_callback_test.py
+++ simple_framework/tests/csv_callback_test.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import mock
 import pandas as pd
 
 import collections
@@ -13,72 +12,7 @@ import shutil
 
 from simple_framework.tests.Tests_base import getSimpleModel, getSimpleDataset, getParameters
 from simple_framework.trainer.trainer import Trainer
-from simple_framework.callbacks.CheckpointCallback import CheckpointCallback
 from simple_framework.callbacks.CsvCallback import CsvCallback
-
-"""
-2 epochs, 10 steps each
-"""
-
-
-@pytest.mark.parametrize(
-    "backbone,frequency, callback_type, expected",
-    [
-        ("Horovod", 1, "step", 20),
-        ("Horovod", 1, "epoch", 2),
-        ("Horovod", 2, "step", 10),
-        ("Horovod", 2, "epoch", 1),
-        ("Horovod", 10, "step", 2),
-        ("Horovod", 10, "epoch", 0),
-        ("Simple", 1, "step", 20),
-        ("Simple", 1, "epoch", 2),
-        ("Simple", 2, "step", 10),
-        ("Simple", 2, "epoch", 1),
-        ("Simple", 10, "step", 2),
-        ("Simple", 10, "epoch", 0),
-    ],
-)
-def test_checkpoint_callback(backbone, frequency, callback_type, expected):
-    tmp_dir = "saved_files"
-
-    if os.path.exists(tmp_dir):
-        shutil.rmtree(tmp_dir)
-
-    Path(tmp_dir).mkdir(parents=True, exist_ok=True)
-
-    model = getSimpleModel()
-    params = getParameters(backbone)
-    dataset = getSimpleDataset()
-
-    trainer = Trainer(model=model, cfg=params)
-
-    checkpoint_callback = CheckpointCallback(
-        save_dir=tmp_dir, frequency=frequency, type=callback_type, save_last=False, save_best=False
-    )
-
-    trainer.fit(
-        train_dataset=dataset,
-        batch_size=1,
-        epochs=2,
-        validation_dataset=None,
-        validation_metric="acc",
-        steps_per_epoch=10,
-        callbacks=[checkpoint_callback],
-    )
-
-    file_number = sum([len(files) for r, d, files in os.walk(tmp_dir)])
-
-    assert file_number == expected
-
-    shutil.rmtree(tmp_dir)
-
-
-# ---------------------------- CsvCallback Test ----------------------------
-
-"""
-2 epochs, 10 steps each
-5 steps validation
-"""
 
 
 @pytest.mark.parametrize(
@@ -110,6 +44,10 @@ def test_csv_callback(
     expected_file_names: List,
     expected_csv_rows_nr: List,
 ):
+    """
+    2 epochs, 10 steps each
+    5 steps validation
+    """
     tmp_dir = "saved_files"
 
     if os.path.exists(tmp_dir):
